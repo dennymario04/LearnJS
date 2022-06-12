bfdc0c236b020855626734877ccd6250393bdc68
dennymario04 <denny.mario.business@gmail.com>
2022-06-12 23:24:42 +0700

    Initial Commit Belajar JS

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+function MyIntro(){
+    let Nama = "Denny Mario";
+    document.getElementById("intro").innerHTML = Nama;
+}
+console.log(MyIntro());
+
+document.getElementById("main").textContent = "Ngulik ngulik disini";
