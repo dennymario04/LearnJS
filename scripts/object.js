468e86ddb92c37de0bc8f4f1542c677a07a78ead
dennymario04 <denny.mario.business@gmail.com>
2022-06-13 22:09:59 +0700

    Larihan Object JS Sedrhana

--- scripts/object.js
+++ scripts/object.js
@@ -10,5 +10,25 @@ const biodata ={
 }
 nama = biodata.fullName();
 console.log(nama);
-
 document.getElementById("intro").textContent = nama;
+
+//Accessors(Getter and Setter)
+
+const infoTambahan={
+    lulusan : "",
+    kelas : "IK 1801",
+    pekerjaaan : "EDP OPS",
+    hobby : "Renang",
+    get pekerjaaan(){
+        return this.pekerjaaan;
+    },
+    set info(lulusan){
+        this.lulusan = lulusan;
+    },
+    printAll : function(){
+        return "Asal Kampus : "+this.lulusan+",\n Kelas: "+this.kelas;
+    }
+}
+infoTambahan.info ="LP3I Binjai";
+cekData = infoTambahan.printAll();
+console.log(cekData)
